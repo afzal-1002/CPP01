debc1b3eb363ff37719ae398b70758ea12e7bbb7
Muhammad Afzal <mafzal@c3r16s2.42warsaw.pl>
2026-06-20 00:56:47 +0200

    Ex5 Ex6 Solved

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mafzal < mafzal@student.42warsaw.pl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 23:26:38 by mafzal            #+#    #+#             */
-/*   Updated: 2026/06/19 23:26:39 by mafzal           ###   ########.fr       */
+/*   Updated: 2026/06/20 00:53:59 by mafzal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 int	main(void)
 {
 	Zombie	*heapZombie;
-	delete	heapZombie;
 
 	heapZombie = newZombie("Foo");
 	heapZombie->announce();
 	randomChump("Bob");
+
+	delete	heapZombie;
 	return (0);
 }
